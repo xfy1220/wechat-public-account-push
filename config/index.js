00847e007287597f08f044eeaecbb57671f70413
xfy1220 <112338543+xfy1220@users.noreply.github.com>
2022-08-29 01:29:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ export const config = {
     // 公众号APP_SECRET
     APP_SECRET: "62f39b0489fdf834a4d85fb5396d38a2",
     // 模板消息id
-    TEMPLATE_ID: "RrBc1wMBHoEJD8R5cIOTFUi5i6qaYKfxMJzaJ8jm4m4",
+    TEMPLATE_ID: "itM08SOXs4dznS8TjoDCsoe8p1G9zh8GEV28YQ2WW_g",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     USERS: ["oJaLp5_addfCYRhZQJ0Jm0jXZY0w,oJaLp54K2FYj1TPEh_mWEzLp485c"],
      
